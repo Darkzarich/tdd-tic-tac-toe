44317b52bf2b44403ad78610575b9f50b4033345
Darkzarich <darkzarichv2@gmail.com>
2021-07-25 20:47:11 +0300

    add game init

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log('test');
+import DomController from './DomController'
+import Game from './Game'
+
+const game = new Game()
+const dom = new DomController({
+  root: 'body', 
+  game
+})
+
+dom.init()
