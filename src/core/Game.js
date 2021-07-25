b2951e3f836226a07887c11d55799bf7e945dc79
Darkzarich <darkzarichv2@gmail.com>
2021-07-25 17:24:03 +0300

    add move history

--- src/core/Game.js
+++ src/core/Game.js
@@ -1,7 +1,10 @@
 export default class Game {
   constructor() {
+    this._userName = 'User';
+    this._computerName = 'Computer';
     this._userMoveSymbol = 'X';
     this._AIMoveSymbol = 'O';
+    this._history = [];
     this._board = [
       ['', '', ''],
       ['', '', ''],
@@ -26,11 +29,21 @@ export default class Game {
   }
 
   makeUserMove(x, y) {
+    this._updateHistory(x, y, this._userName);
     this._updateBoard(x, y, this._userMoveSymbol);
   }
 
   makeAIMove(x, y) {
+    this._updateHistory(x, y, this._computerName);
     this._updateBoard(x, y, this._AIMoveSymbol);
+  }
+
+  getMoveHistory() {
+    return this._history;
+  }
+
+  _updateHistory(x, y, name) {
+    this._history.push({ turn: name, x, y });
   }
 
   _throwException(msg) {
--- test/game.js
+++ test/game.js
@@ -1,6 +1,8 @@
 import { expect } from 'chai';
 import Game from '../src/core/Game';
 
+const userName = 'User';
+const aiName = 'Computer';
 const userMoveSymbol = 'X';
 const AIMoveSymbol = 'O';
 const initialGameBoard = [
@@ -53,5 +55,45 @@ describe('Game', () => {
     const board = game.getState();
 
     expect(board[x][y]).to.equal(AIMoveSymbol);
+  });
+
+  it("Game saves user's move in history", () => {
+    const x = 1,
+      y = 1;
+
+    game.makeUserMove(x, y);
+
+    const history = game.getMoveHistory();
+
+    expect(history).to.deep.equal([{ turn: userName, x, y }]);
+  });
+
+  it("Game saves computers's move in history", () => {
+    const x = 1,
+      y = 1;
+
+    game.makeAIMove(x, y);
+
+    const history = game.getMoveHistory();
+
+    expect(history).to.deep.equal([{ turn: aiName, x, y }]);
+  });
+
+  it('Game saves long move history', () => {
+    game.makeAIMove(0, 0);
+    game.makeUserMove(0, 1);
+    game.makeAIMove(1, 1);
+    game.makeUserMove(0, 2);
+
+    const history = game.getMoveHistory();
+
+    expect(history.length).to.equal(4);
+
+    expect(history).to.deep.equal([
+      { turn: aiName, x: 0, y: 0 },
+      { turn: userName, x: 0, y: 1 },
+      { turn: aiName, x: 1, y: 1 },
+      { turn: userName, x: 0, y: 2 },
+    ]);
   });
 });
